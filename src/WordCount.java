df3bbb4be4a872481dc245ea5dd12b4092da191b
ManuelaBarth <manubarth.jippie8390@gmail.com>
2024-01-11 15:31:52 +0100

    refactoring before adding index
    added new constructor for WordCount

--- src/WordCount.java
+++ src/WordCount.java
@@ -3,32 +3,36 @@ import java.util.Arrays;
 public class WordCount {
 
   private final String[] stopWords = {"the", "a", "on", "off"};
+  private final String initialText;
 
-  public WordCountingResult countWords(String text) {
-    String[] cleanedListOfWords = getCleanedListOfWords(text);
-    int nrOfUniqueWords = getNumberOfUniqueWords(cleanedListOfWords);
-
-    WordCountingResult result = new WordCountingResult(cleanedListOfWords.length, nrOfUniqueWords);
-    return result;
+  public WordCount(String text) {
+    initialText = text;
   }
 
-  private String[] getCleanedListOfWords(String text) {
-    return Arrays.stream(getWords(text))
+  public WordCountingResult countWords() {
+    String[] cleanedListOfWords = getCleanedListOfWords();
+    int nrOfUniqueWords = getNumberOfUniqueWords(cleanedListOfWords);
+
+    return new WordCountingResult(cleanedListOfWords.length, nrOfUniqueWords);
+  }
+
+  private String[] getCleanedListOfWords() {
+    return Arrays.stream(getWords())
         .filter(x -> !Arrays.asList(stopWords).contains(x))
         .filter(x -> !x.isBlank())
         .toArray(String[]::new);
   }
 
-  private String[] getWords(String text) {
-    return text.split("[^A-Za-z\\-]");
+  private String[] getWords() {
+    return initialText.split("[^A-Za-z\\-]");
   }
 
   private int getNumberOfUniqueWords(String[] cleanedText) {
     return Arrays.stream(cleanedText).sorted().distinct().toArray().length;
   }
 
-  public double getAverageWordLength(String text) {
-    String[] words = getWords(text);
+  public double getAverageWordLength() {
+    String[] words = getWords();
     if (words.length == 0) {
       return 0;
     }
--- test/WordCountTest.java
+++ test/WordCountTest.java
@@ -5,25 +5,25 @@ public class WordCountTest {
 
   @Test
   public void singleLetterTest() {
-    Assert.assertEquals(1, new WordCount().countWords("p").getCountedWords());
+    Assert.assertEquals(1, new WordCount("p").countWords().getCountedWords());
   }
 
   @Test
   public void simpleSentenceTest() {
-    Assert.assertEquals(4, new WordCount().countWords("Mary had a little lamb").getCountedWords());
+    Assert.assertEquals(4, new WordCount("Mary had a little lamb").countWords().getCountedWords());
   }
 
   @Test
   public void allStopWordsTest() {
-    Assert.assertEquals(0, new WordCount().countWords("the a on off").getCountedWords());
+    Assert.assertEquals(0, new WordCount("the a on off").countWords().getCountedWords());
   }
 
   @Test
   public void humptyNumberTest() {
     Assert.assertEquals(
         7,
-        new WordCount()
-            .countWords("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.")
+        new WordCount("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.")
+            .countWords()
             .getCountedWords());
   }
 
@@ -31,23 +31,23 @@ public class WordCountTest {
   public void humptyUniqueTest() {
     Assert.assertEquals(
         6,
-        new WordCount()
-            .countWords("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.")
+        new WordCount("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.")
+            .countWords()
             .getUniqueWords());
   }
 
   @Test
   public void averageLengthOneWord() {
-    Assert.assertEquals(4, new WordCount().getAverageWordLength("ball"), 0.01);
+    Assert.assertEquals(4, new WordCount("ball").getAverageWordLength(), 0.01);
   }
 
   @Test
   public void averageLengthWithDigits() {
-    Assert.assertEquals(3.5, new WordCount().getAverageWordLength("ball out"), 0.01);
+    Assert.assertEquals(3.5, new WordCount("ball out").getAverageWordLength(), 0.01);
   }
 
   @Test
   public void averageLengthEmptyWord() {
-    Assert.assertEquals(0d, new WordCount().getAverageWordLength(" "), 0.01);
+    Assert.assertEquals(0d, new WordCount(" ").getAverageWordLength(), 0.01);
   }
 }
