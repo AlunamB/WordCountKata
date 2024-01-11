71fe54957346eae3eaa7a1c796e9289516949ce9
ManuelaBarth <manubarth.jippie8390@gmail.com>
2024-01-11 10:39:57 +0100

    kata3, preparation for unique words requirement

--- src/WordCount.java
+++ src/WordCount.java
@@ -1,16 +1,23 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class WordCount {
 
   private final String[] stopWords = {"the", "a", "on", "off"};
 
-  public int countWords(String text) {
-    String[] words = text.split(" ");
+  public List<Long> countWords(String text) {
+    List<Long> result = new ArrayList<>();
+
+    String[] words = text.split("[^A-Za-z]");
     String[] cleanedText =
         Arrays.stream(words)
             .filter(x -> !Arrays.asList(stopWords).contains(x))
+            .filter(x -> !x.isBlank())
             .toArray(String[]::new);
 
-    return cleanedText.length;
+    System.out.println(cleanedText.toString());
+    result.add(Long.valueOf(cleanedText.length));
+    return result;
   }
 }
--- test/WordCountTest.java
+++ test/WordCountTest.java
@@ -5,16 +5,35 @@ public class WordCountTest {
 
   @Test
   public void singleLetterTest() {
-    Assert.assertEquals(1, new WordCount().countWords("p"));
+    Assert.assertEquals(1, new WordCount().countWords("p").get(0).longValue());
   }
 
   @Test
   public void simpleSentenceTest() {
-    Assert.assertEquals(4, new WordCount().countWords("Mary had a little lamb"));
+    Assert.assertEquals(4, new WordCount().countWords("Mary had a little lamb").get(0).longValue());
   }
 
   @Test
   public void allStopWordsTest() {
-    Assert.assertEquals(0, new WordCount().countWords("the a on off"));
+    Assert.assertEquals(0, new WordCount().countWords("the a on off").get(0).longValue());
   }
+
+  @Test
+  public void HumptyNumberTest() {
+    Assert.assertEquals(
+        9,
+        new WordCount()
+            .countWords("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall.")
+            .get(0)
+            .longValue());
+  }
+
+  /* @Test
+  public void HumptyUniqueTest() {
+    Assert.assertEquals(
+        7,
+        new WordCount().countWords("Humpty-Dumpty sat on a wall. Humpty-Dumpty had a great fall."));
+  }
+  */
+
 }
